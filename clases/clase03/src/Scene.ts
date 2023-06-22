45e361c1f822110e70ed284194ed21baabc3f846
Darkzalodev <gonza1810@gmail.com>
2023-06-22 16:27:37 -0300

    Agregando AnimatedSprite de un chica caminando

--- clases/clase03/src/Scene.ts
+++ clases/clase03/src/Scene.ts
@@ -1,4 +1,4 @@
-import { Container } from 'pixi.js';
+import { Container, AnimatedSprite, Texture } from 'pixi.js';
 import DinoWithHat from './DinoWithHat';
 
 class Scene extends Container {
@@ -7,7 +7,26 @@ class Scene extends Container {
     const Dino = new DinoWithHat();
 
     console.log('DinoWithHat Position: ', Dino.position.x, Dino.position.y);
+
+    const girlAnimated = new AnimatedSprite(
+      [
+        Texture.from('girl1'),
+        Texture.from('girl2'),
+        Texture.from('girl3'),
+        Texture.from('girl4'),
+        Texture.from('girl5'),
+        Texture.from('girl6'),
+        Texture.from('girl7'),
+        Texture.from('girl8'),
+      ],
+      true
+    );
+
+    girlAnimated.play();
+    girlAnimated.animationSpeed = 0.2;
+
     this.addChild(Dino);
+    this.addChild(girlAnimated);
   }
 }
 
--- clases/clase03/src/assets.ts
+++ clases/clase03/src/assets.ts
@@ -1,4 +1,12 @@
 export const assets = {
   Hat: 'hat.png',
   Dino: 'dino.png',
+  girl1: 'girl1.png',
+  girl2: 'girl2.png',
+  girl3: 'girl3.png',
+  girl4: 'girl4.png',
+  girl5: 'girl5.png',
+  girl6: 'girl6.png',
+  girl7: 'girl7.png',
+  girl8: 'girl8.png',
 };
